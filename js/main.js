6618c73db72739111b375b78f4e539c1bcb12be8
jacopo-derrico <derricoj.design@gmail.com>
2024-01-09 14:46:39 +0100

    workspace setup w/ notes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//NOTE - 1 - print number from 1 to 100; 
+//2 - multiples of 3 print "Fizz"; 
+//3 - multiples of 5 print "Buzz"; 
+//4 - multiples of 3 and 5 print "FizzBuzz"
+//5 - BONUS use append() to create container with boxes w/ numbers inside
+//6 - BONUS add differents styles to every box
+
